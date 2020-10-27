3a5a140a6c51456d7f26b98b61397de5d13cff00
Stuart Wong <swong31@gmail.com>
2020-10-27 16:54:32 -0700

    Created models folder with index.js; completed Workout.js with virtuals

--- models/Workout.js
+++ models/Workout.js
@@ -1,19 +1,52 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-    day: {
+const opts = { toJSON: { virtuals: true } };
 
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now()
+  },
+  exercises: [
+    {
+     type: {
+         type: String,
+         required: "Exercise Type is Required"
+     },
+     name: {
+         type: String,
+         trim: true,
+         required: "Name of Exercise is Required"
+     },
+     duration: {
+         type: Number,
+         required: "Duration is Required"
+     },
+     weight: {
+         type: Number
+     },
+     reps: {
+         type: Number
+     },
+     sets: {
+         type: Number
+     },
+     distance: {
+         type: Number
+     }
     },
-    exercises: [
-        {
-        type: 
-        name:
-        duration:
-        distance:
-        weight:
-        reps:
-        sets:
+  ],
+}, opts);
+
+WorkoutSchema.virtual('totalDuration').get(function() {
+    let sum = 0;
+    for (let index = 0; index < exercises.length; index++) {
+        sum += exercises.duration[index];
     }
-]
-});
+    return sum;
+  });
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
