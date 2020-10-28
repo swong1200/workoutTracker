c8b40870918a80e0c52951c0aadd685435a80a66
Stuart Wong <swong31@gmail.com>
2020-10-27 21:43:09 -0700

    Added html routes to server.js

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -9,38 +9,38 @@ router.get("/", function(req, res) {
 
 });
 
-// getLastWorkout()
-app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+// // getLastWorkout()
+// app.get("/api/workouts", (req, res) => {
+//     db.Workout.find({})
+//       .then(dbWorkout => {
+//         res.json(dbWorkout);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
   
-  // addExercise()
-  app.put("/api/workouts/:id", (req, res) => {
-    db.Workout.
-  });
+//   // addExercise()
+//   app.put("/api/workouts/:id", (req, res) => {
+//     db.Workout.
+//   });
   
-  // createWorkout()
-  app.post("/api/workouts", ({ body }, res) => {
-    db.Workout.create(body)
-      // .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-      // .then(dbWorkout => {
-      //   res.json(dbWorkout);
-      // })
-      // .catch(err => {
-      //   res.json(err);
-      // });
-  });
+//   // createWorkout()
+//   app.post("/api/workouts", ({ body }, res) => {
+//     db.Workout.create(body)
+//       // .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
+//       // .then(dbWorkout => {
+//       //   res.json(dbWorkout);
+//       // })
+//       // .catch(err => {
+//       //   res.json(err);
+//       // });
+//   });
   
-  // getWorkoutsInRange()
-  app.get("/api/workouts/range", (req, res) => {
-    db.Workout.
-  });
+//   // getWorkoutsInRange()
+//   app.get("/api/workouts/range", (req, res) => {
+//     db.Workout.
+//   });
 
 // Export routes for server.js to use
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 const db = require("./models");
 
 const app = express();
+
+const routes = require("./controllers/workoutController");
 
 app.use(logger("dev"));
 
@@ -17,6 +20,24 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.use(routes);
+
+// Index Route
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// Exercise Route
+app.get("/exercise", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// Stats Route
+app.get("/stats", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
 
 
 
