ba8cd3db30aa2e93355743f81e583eb12185166c
Stuart Wong <swong31@gmail.com>
2020-10-30 19:32:56 -0700

    Final commit for deadline

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{ 
+  useNewUrlParser: true, 
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}
+);
 
 // API Routes
 app.use(routes);
